eb5721caca7c1d04bdaf5f2a3ade1bbbd25e0172
guoyonglei <guoyonglei@thinkit.cn>
2021-03-01 10:31:07 +0800

    Qt http 安全模式

--- QtExample/WidgetNetWork.cpp
+++ QtExample/WidgetNetWork.cpp
@@ -2,11 +2,40 @@
 
 WidgetNetWork::WidgetNetWork(QWidget* parent) : QWidget(parent)
 {
+	m_hBoxLayout->addStretch(1);
+	m_hBoxLayout->addWidget(m_pushButton);
 	m_hBoxLayout->addWidget(m_label);
+	m_hBoxLayout->addWidget(m_textEdit);
+	m_hBoxLayout->addStretch(1);
+	m_textEdit->setFixedSize(100, 100);
 	setLayout(m_hBoxLayout);
+	connect(m_pushButton, SIGNAL(clicked()), this, SLOT(onPushButtonClicked()));
+
+	m_NetworkNanager = new QNetworkAccessManager(this);
+	connect(m_NetworkNanager, SIGNAL(finished(QNetworkReply*)), this, SLOT(replyFinished(QNetworkReply*)));
 }
 
 WidgetNetWork::~WidgetNetWork()
 {
+
+}
+
+void WidgetNetWork::onPushButtonClicked()
+{
+	//m_label->setText(QStringLiteral("¹þ¹þ¹þ"));
+	m_NetworkNanager->get(QNetworkRequest(QUrl("https://www.baidu.com")));
+}
+
+
+void WidgetNetWork::replyFinished(QNetworkReply* reply)
+{
+	//QTextCodec *codec = QTextCodec::codecForLocale();
+	//QString all = codec->toUnicode(reply->readAll().toStdString());
+	QString all = QString::fromStdString(reply->readAll().toStdString());
+	qDebug() << all;
+	//qDebug() << reply->readAll();
+	//m_label->setText(all);
+	m_textEdit->setText("123" + all + "456");
+	reply->deleteLater();
 
 }
--- QtExample/WidgetNetWork.h
+++ QtExample/WidgetNetWork.h
@@ -2,6 +2,12 @@
 #include <QWidget>
 #include <QLabel>
 #include <QHBoxLayout>
+#include <QPushButton>
+#include <QtNetwork/QNetworkReply>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QTextCodec>
+#include <QTextBrowser>
+#include <QTextEdit>
 
 class WidgetNetWork : public QWidget
 {
@@ -10,8 +16,18 @@ public:
 	explicit WidgetNetWork(QWidget* parent = nullptr);
 	~WidgetNetWork();
 
+private slots:
+	void onPushButtonClicked();
+	void replyFinished(QNetworkReply*);
+
 private:
 	QLabel* m_label = new QLabel(QStringLiteral("ÍøÂç"));
+	QPushButton* m_pushButton = new QPushButton;
 	QHBoxLayout* m_hBoxLayout = new QHBoxLayout;
+	//QTextBrowser* m_textBrowser = new QTextBrowser;
+	QTextEdit* m_textEdit = new QTextEdit;
+
+private:
+	QNetworkAccessManager *m_NetworkNanager;
 };
 
